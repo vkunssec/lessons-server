0f5bd41d09e3efc995697349333e3ee33b96e011
Victor Martins <vkunssec@gmail.com>
2019-10-25 00:14:49 -0300

    add gitlab ci

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 443;
 
-const opts = {
+const credentials = {
 	key: fs.readFileSync('/etc/letsencrypt/live/vknc.tk/privkey.pem'),
 	cert: fs.readFileSync('/etc/letsencrypt/live/vknc.tk/fullchain.pem')
 };
 
 app.use('/', express.static('./public'));
 
-https.createServer(opts, app).listen(port, () => console.log(`Listening port ${port}.`));
+https.createServer(credentials, app).listen(port, () => console.log(`Listening port ${port}.`));
