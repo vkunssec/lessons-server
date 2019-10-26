8ebc6604abc79373e4d1bba8d8e8c1a81561655c
Victor Martins <vkunssec@gmail.com>
2019-10-26 16:34:57 -0300

    effect on scroll and onload page

--- static/public/assets/js/main.js
+++ static/public/assets/js/main.js
@@ -1,16 +1,57 @@
 document.addEventListener('DOMContentLoaded', () => {
-    (() => {
-        const elems = document.querySelectorAll(`[fade-in]`);
-        elems.forEach((el) => { 
-            el.classList.add('onview'); 
-        });
-    })(setTimeout(1000));
+    var vknc = document.querySelector(`main > .vknc[fade-in]`);
+    var square = document.querySelector(`main > .square[fade-in]`);
+
+    function fadeInAnimVknc() {
+
+        var positionTop = 0;
+        var paddingLeft = 0;
+        var opacity = 0.0;
+
+        function anim() {
+            vknc.style.top = `${positionTop*2.5}px`;
+            vknc.style.opacity = `${opacity}`;
+            vknc.style.paddingLeft = `${paddingLeft}%`;
+            positionTop++;
+            paddingLeft = paddingLeft + 0.07;
+            opacity = opacity + 0.01;
+            if (positionTop > 100) cancelAnimationFrame(idAFVknc);
+            else idAFVknc = requestAnimationFrame(anim);
+        }
+
+        var idAFVknc = requestAnimationFrame(anim);
+
+    }
+    
+    function fadeInAnimSquare() {
+
+        var positionRight = -363;
+        var opacity = 0.0;
+
+        function anim() {
+            square.style.right = `${positionRight}px`;
+            square.style.opacity = `${opacity}`;
+            positionRight++;
+            opacity = opacity + 0.01;
+            if (positionRight === -121 || opacity > 1) cancelAnimationFrame(idAFSquare);
+            else idAFSquare = requestAnimationFrame(anim);
+        }
+
+        var idAFSquare = requestAnimationFrame(anim);
+    }
+
+    fadeInAnimVknc();
+    fadeInAnimSquare();
+
 });
 
-document.addEventListener('scroll', (e) => {
-    var body = document.querySelector('body');
-    var bodyRect = body.getBoundingClientRect();
+document.addEventListener('scroll', () => {
+    let body = document.querySelector('body');
+    let bodyRect = body.getBoundingClientRect();
 
-    var el = document.querySelector(`main > .vknc`);
-    el.style.top = `${200 - bodyRect.y + (bodyRect.y)*0.5}px`;
+    let vknc = document.querySelector(`main > .vknc`);
+    vknc.style.top = `${250 - bodyRect.y + (bodyRect.y*0.5)}px`;
+
+    let square = document.querySelector(`main > .square`);
+    square.style.top = `${128 + (bodyRect.y*0.1)}px`;
 });
