53dc897a246674027178b3d39f992cf5f0dfacf4
Victor Martins <vkunssec@gmail.com>
2019-10-26 09:59:35 -0300

    efect parallax test

--- static/public/assets/js/main.js
+++ static/public/assets/js/main.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const elems = document.querySelectorAll(`[fade-in]`);
-    elems.forEach((el) => { el.classList.add('onview'); });
+    (() => {
+        const elems = document.querySelectorAll(`[fade-in]`);
+        elems.forEach((el) => { 
+            el.classList.add('onview'); 
+        });
+    })(setTimeout(1000));
+});
+
+document.addEventListener('scroll', (e) => {
+    var body = document.querySelector('body');
+    var bodyRect = body.getBoundingClientRect();
+
+    var el = document.querySelector(`main > .vknc`);
+    el.style.top = `${200 - bodyRect.y + (bodyRect.y)*0.5}px`;
 });
