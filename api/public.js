54bfce86b5efb63d13858dba27a3b9b47580c79b
Victor Martins <vkunssec@gmail.com>
2019-11-11 12:29:49 -0300

    config static page dist path

--- api/public.js
+++ api/public.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use('/', express.static('static/public'));
-app.use('/jetlag', express.static('static/jetlag'));
+app.use('/jetlag', express.static('static/jetlag/dist'));
 
 module.exports = app;
