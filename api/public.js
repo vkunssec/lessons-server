deed05078b6deae8e068e31ed6d0c159df4e0eca
Victor Martins AWS Server Lightsail <vkunssec@gmail.com>
2019-11-11 12:54:25 +0000

    jetlag depend

--- api/public.js
+++ api/public.js
@@ -2,5 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use('/', express.static('static/public'));
+app.use('/jetlag', express.static('static/jetlag'));
 
 module.exports = app;
